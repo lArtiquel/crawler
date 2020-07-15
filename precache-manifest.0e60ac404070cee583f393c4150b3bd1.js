07a84856a49069f6792cc8e708bf791bbaad939c
Artyom <artem.tsvirko@mail.ru>
2020-07-15 13:33:15 +0300

    Updates

---
+++ precache-manifest.0e60ac404070cee583f393c4150b3bd1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab741a062131392451540d293135fc9e",
+    "url": "/crawler/index.html"
+  },
+  {
+    "revision": "2800faf593691c575f2e",
+    "url": "/crawler/static/css/main.4013d035.chunk.css"
+  },
+  {
+    "revision": "7d63c621dd205e27c029",
+    "url": "/crawler/static/js/2.37e66418.chunk.js"
+  },
+  {
+    "revision": "c80b09f21c3dd2137be347c65d78f97d",
+    "url": "/crawler/static/js/2.37e66418.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2800faf593691c575f2e",
+    "url": "/crawler/static/js/main.cfe22ba1.chunk.js"
+  },
+  {
+    "revision": "7361a71409da6d21b72a",
+    "url": "/crawler/static/js/runtime-main.4ba32193.js"
+  },
+  {
+    "revision": "db883515e3c1c197b88826cb87c2c1df",
+    "url": "/crawler/static/media/cool-spider.db883515.png"
+  }
+]);
